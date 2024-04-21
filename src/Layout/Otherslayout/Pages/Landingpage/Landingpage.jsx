4ff8d30512c732cbb854478139271fbb8fbf46a0
Atik Rahad <atikrahad1@gmail.com>
2024-04-21 12:59:51 +0600

    adding servises in landingpage

--- src/Layout/Otherslayout/Pages/Landingpage/Landingpage.jsx
+++ src/Layout/Otherslayout/Pages/Landingpage/Landingpage.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Components/Banner"
 import Landingpagenavber from "../../Components/Landingpagenavber"
+import Ourservises from "../../Components/Ourservices"
 
 const Landingpage = () => {
   return (
     <div>
       <Landingpagenavber></Landingpagenavber>
       <Banner></Banner>
+      <Ourservises></Ourservises>
     </div>
   )
 }
