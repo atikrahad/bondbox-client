4f129eeb4c1721c483ef985be28c6258006c927b
Atik Rahad <atikrahad1@gmail.com>
2024-04-21 13:04:32 +0600

    adding footer in landing page

--- src/Layout/Otherslayout/Pages/Landingpage/Landingpage.jsx
+++ src/Layout/Otherslayout/Pages/Landingpage/Landingpage.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../Components/Banner"
+import Footer from "../../Components/Footer"
 import Givefeadback from "../../Components/Givefeedback"
 import Landingpagenavber from "../../Components/Landingpagenavber"
 import Ourservises from "../../Components/Ourservices"
@@ -10,6 +11,7 @@ const Landingpage = () => {
       <Banner></Banner>
       <Ourservises></Ourservises>
       <Givefeadback></Givefeadback>
+      <Footer></Footer>
     </div>
   )
 }
